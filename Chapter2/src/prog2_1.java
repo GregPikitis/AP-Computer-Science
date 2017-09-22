8480c4f82c725cab4cc4a8b91911c55fdc565c85
Greg Pikitis <egblip@gmail.com>
2017-09-22 13:25:56 -0400

    Adding things!!!

---
+++ Chapter2/src/prog2_1.java
@@ -0,0 +1,11 @@
+public class prog2_1
+{
+    public static void main (String [] args) {
+
+        // Write a program that displays your name, address, and telephone number
+
+        System.out.println("Name");
+        System.out.println("Address");
+        System.out.println("Phone Number");
+    }
+}
---
+++ Chapter2/src/prog2_2.java
@@ -0,0 +1,25 @@
+public class prog2_2
+{
+    public static void main (String [] args) {
+
+        // A yield sign encloses the word YIELD within a triangle.
+        // Write a program that displays a yield sign.
+        // (Use stars to represent the sides of the triangle.)
+
+        System.out.println("             *             ");
+        System.out.println("            * *            ");
+        System.out.println("           *   *           ");
+        System.out.println("          *     *          ");
+        System.out.println("         *       *         ");
+        System.out.println("        *         *        ");
+        System.out.println("       *           *       ");
+        System.out.println("      *             *      ");
+        System.out.println("     *    Yield!!    *     ");
+        System.out.println("    *                 *    ");
+        System.out.println("   *                   *   ");
+        System.out.println("  *                     *  ");
+        System.out.println(" *                       * ");
+        System.out.println("* * * * * * * * * * * * * *");
+
+    }
+}
